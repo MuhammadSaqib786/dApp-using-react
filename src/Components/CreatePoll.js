5e09475ef4d92fe0f2662efe42bbea57ed3a3158
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-07-02 23:06:08 -0700

    added form controls
    onChange={event()=>{setName(event.target .value)}}
    
    this function with every input

--- src/Components/CreatePoll.js
+++ src/Components/CreatePoll.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
@@ -17,24 +17,44 @@ const useStyles = makeStyles((theme) => ({
 
 export const CreatePoll = () => {
   const classes = useStyles();
+  //now for input control
+  const [question, setQuestion] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [image, setImage] = useState("");
   return (
     <div style={{ marginTop: 30 }}>
-      <form className={classes.root}>
+      <form className={classes.root} method="get">
         <table style={{ width: "100%", backgroundColor: "white" }}>
           <tr>
             <Input
               defaultValue="Write Question Here"
               inputProps={{ "aria-label": "description" }}
               id="question"
+              name="question"
               style={{ width: "50%", marginBottom: 20 }}
+              onChange={(event) => setQuestion(event.target.value)}
             />
           </tr>
-          <tr style={{ marginTop: 10 }}>
+          <tr>
+            <Input
+              defaultValue="Url to image"
+              inputProps={{ "aria-label": "description" }}
+              id="imageUrl"
+              name="imageUrl"
+              style={{ width: "30%" }}
+              onChange={(event) => setImage(event.target.value)}
+            />
+          </tr>
+          <tr>
             <Input
               defaultValue="Option 1"
               inputProps={{ "aria-label": "description" }}
               id="option1"
+              name="option1"
               style={{ width: "30%" }}
+              onChange={(event) => setOption1(event.target.value)}
             />
           </tr>
           <tr>
@@ -42,7 +62,9 @@ export const CreatePoll = () => {
               defaultValue="Option 2"
               inputProps={{ "aria-label": "description" }}
               id="option2"
+              name="option2"
               style={{ width: "30%" }}
+              onChange={(event) => setOption2(event.target.value)}
             />
           </tr>
           <tr>
@@ -50,7 +72,9 @@ export const CreatePoll = () => {
               defaultValue="Option 3"
               inputProps={{ "aria-label": "description" }}
               id="option3"
+              name="option3"
               style={{ width: "30%" }}
+              onChange={(event) => setOption3(event.target.value)}
             />
           </tr>
           <tr style={{ align: "center" }}>
@@ -59,8 +83,9 @@ export const CreatePoll = () => {
               color="primary"
               className={classes.button}
               style={{ marginTop: 10 }}
+              type="submit"
             >
-              Add
+              Submit Poll
             </Button>
           </tr>{" "}
         </table>
